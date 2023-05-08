51af7324b1fd6ffe82e385734e124ea63e88451f
Till Skrodzki <till@mueskro.de>
2023-05-08 16:41:04 +0200

    Add close() for WebsocketServer.py

--- mapadroid/websocket/WebsocketServer.py
+++ mapadroid/websocket/WebsocketServer.py
@@ -71,8 +71,7 @@ class WebsocketServer(object):
 
         # asyncio loop for the entire server
         self.__loop: Optional[asyncio.AbstractEventLoop] = None
-        self.__loop_tid: int = -1
-        self.__server_task = None
+        self.__server_task: Optional[websockets.WebSocketServer] = None
 
     async def __setup_first_loop(self):
         logger.debug("Device mappings: {}", await self.__mapping_manager.get_all_devicemappings())
@@ -87,9 +86,9 @@ class WebsocketServer(object):
         await self.__setup_first_loop()
         # the type-check here is sorta wrong, not entirely sure why
         # noinspection PyTypeChecker
-        await websockets.serve(self.__connection_handler, self.__args.ws_ip, int(self.__args.ws_port), max_size=2 ** 25,
-                               close_timeout=10)
-        self.__loop_tid = current_thread()
+        self.__server_task = await websockets.serve(self.__connection_handler, self.__args.ws_ip,
+                                                    int(self.__args.ws_port), max_size=2 ** 25,
+                                                    close_timeout=10)
 
     async def __close_all_connections_and_signal_stop(self):
         logger.info("Signaling all workers to stop")
@@ -110,9 +109,9 @@ class WebsocketServer(object):
             await asyncio.sleep(1)
 
         await self.__close_all_connections_and_signal_stop()
+        await self.__server_task.close()
         logger.info("Stopped websocket server")
 
-    @logger.catch()
     async def __connection_handler(self, websocket_client_connection: websockets.WebSocketClientProtocol,
                                    path: str) -> None:
         """
@@ -127,7 +126,6 @@ class WebsocketServer(object):
         """
         if self.__stop_server.is_set():
             return
-        # check auth and stuff TODO
         origin: Optional[str]
         success: Optional[bool]
         (origin, success) = await self.__authenticate_connection(websocket_client_connection)
@@ -208,7 +206,7 @@ class WebsocketServer(object):
 
             if entry:
                 try:
-                    await self.__client_message_receiver(origin, entry)
+                    await self.__client_message_receiver(entry)
                 except CancelledError as e:
                     logger.info("Connection to {} has been cancelled", origin)
                 # also check if thread is already running to not start it again. If it is not alive, we need to create it..
@@ -315,7 +313,7 @@ class WebsocketServer(object):
             return origin, False
         return origin, True
 
-    async def __client_message_receiver(self, origin: str, client_entry: WebsocketConnectedClientEntry) -> None:
+    async def __client_message_receiver(self, client_entry: WebsocketConnectedClientEntry) -> None:
         if client_entry is None:
             return
         connection: websockets.WebSocketClientProtocol = client_entry.websocket_client_connection
